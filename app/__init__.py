4990e2fe58a8ff22bd8fc2772f17fb2bfde62def
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-12 11:13:53 +0300

    updated profile

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
+photos = UploadSet('photos',IMAGES)
 db = SQLAlchemy()
 def create_app(config_name):
 
@@ -17,8 +19,13 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
     
-    db.init_app(app)
+    
     bootstrap.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+       db.create_all()
+    # configure UploadSet
+    configure_uploads(app,photos)
     from .models import User
     login_manager.init_app(app)
 
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     @staticmethod
     def init_app(app):
         pass
