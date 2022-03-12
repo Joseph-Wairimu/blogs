d5a72994e7d47aff00ed9f5f8c33f2577f3ff196
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-12 10:08:16 +0300

    user login

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
+
+
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    email=db.Column(db.String(255),unique = True)
+    password=db.Column(db.String(255))
+    username = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
--- manage.py
+++ manage.py
@@ -1,5 +1,14 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 # Creating app instance
 app = create_app('development')
 manager = Manager(app)
