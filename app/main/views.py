f13679ce1cc77fdb2eebab5eedfd64e62b763279
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-12 11:01:07 +0300

    user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,24 @@
 from . import main
 from flask import render_template,request,redirect,url_for,abort
-from flask_login import login_required
+from flask_login import login_required, current_user
+from .forms import UpdateProfile
+from ..models import  User
 #....
 
 @main.route('/')
 def index():
     
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    if user is None:
+        abort(404)
+    
+    return render_template('profile/profile.html',user = user)       
