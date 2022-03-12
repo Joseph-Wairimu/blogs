da44350712adfda6935e7e73e36cfdc2a489516e
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-12 09:56:46 +0300

    config database URI

---
+++ app/main/views.py
@@ -0,0 +1,7 @@
+from . import main
+from flask import render_template,request,redirect,url_for,abort
+
+@main.route('/')
+def index():
+    
+    return render_template('index.html')
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
 class Config:
 
-  
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
     @staticmethod
     def init_app(app):
         pass
