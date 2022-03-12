4bdc8007e8dae85d30f177373646de0df9ac910c
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-12 10:47:18 +0300

    user login

--- app/__init__.py
+++ app/__init__.py
@@ -19,8 +19,9 @@ def create_app(config_name):
     
     db.init_app(app)
     bootstrap.init_app(app)
+    from .models import User
     login_manager.init_app(app)
-    
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,10 @@
 from flask import render_template,redirect,url_for, request, flash
 from . import auth
+from ..models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .. import db
 
 
 
@@ -12,7 +17,25 @@ def login():
     '''
     
     
-    return render_template('auth/login.html')    
+    return render_template('auth/login.html')   
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+  
+    if user is not None and not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+   
+    login_user(user, remember=remember)
+
+    return redirect(url_for('main.index'))    
+
 
 @auth.route('/signup')
 def signup():
@@ -23,3 +46,39 @@ def signup():
     
     
     return render_template('auth/signup.html')
+
+
+@auth.route('/signup',methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')  
+    
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        flash('Email address already exists.')
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+   
+
+    return redirect(url_for('auth.login'))
+    title = "New Account"    
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    logout_user()
+    return redirect(url_for("main.index"))            
+
